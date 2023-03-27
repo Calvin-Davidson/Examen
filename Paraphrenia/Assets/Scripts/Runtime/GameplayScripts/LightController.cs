6f059601afd9f5c355205eb0586e965c432caace
Calvin-Davidson <calvin2003davidson@gmail.com>
2023-03-27 13:15:02 +0200

    Added namespaces

--- Paraphrenia/Assets/Scripts/Runtime/GameplayScripts/LightController.cs
+++ Paraphrenia/Assets/Scripts/Runtime/GameplayScripts/LightController.cs
@@ -1,38 +1,41 @@
 using UnityEngine;
 
-/// <summary>
-/// This script is a basic controller script that can drive light intensity through an On and Off state.
-/// There is support for a color override, though the light's default configured color can also be used.
-/// This script is meant to be hooked up to the RandomFlicker.cs manager script.
-/// WARNING: When setting intensity through code, you MUST configure the light with the intensity unit "Nits".
-/// </summary>
-
-[RequireComponent(typeof(Light))]
-public class LightController : MonoBehaviour
+namespace Runtime.GameplayScripts
 {
-    [SerializeField] private bool overrideColor = false;
-    [SerializeField] private Color color = Color.white;
-    [SerializeField] private float offIntensity = 5000; 
-    [SerializeField] private float onIntensity = 9000;
+    /// <summary>
+    /// This script is a basic controller script that can drive light intensity through an On and Off state.
+    /// There is support for a color override, though the light's default configured color can also be used.
+    /// This script is meant to be hooked up to the RandomFlicker.cs manager script.
+    /// WARNING: When setting intensity through code, you MUST configure the light with the intensity unit "Nits".
+    /// </summary>
 
-    private Light light;
-
-    private void Awake()
+    [RequireComponent(typeof(Light))]
+    public class LightController : MonoBehaviour
     {
-        light = GetComponent<Light>();
-        if (overrideColor)
+        [SerializeField] private bool overrideColor = false;
+        [SerializeField] private Color color = Color.white;
+        [SerializeField] private float offIntensity = 5000; 
+        [SerializeField] private float onIntensity = 9000;
+
+        private Light light;
+
+        private void Awake()
         {
-            light.color = color;
+            light = GetComponent<Light>();
+            if (overrideColor)
+            {
+                light.color = color;
+            }
         }
-    }
 
-    public void TurnLightOn()
-    {
-        if(light != null) light.intensity = onIntensity;
-    }
+        public void TurnLightOn()
+        {
+            if(light != null) light.intensity = onIntensity;
+        }
 
-    public void TurnLightOff()
-    {
-        if (light != null) light.intensity = offIntensity;
+        public void TurnLightOff()
+        {
+            if (light != null) light.intensity = offIntensity;
+        }
     }
 }
--- Paraphrenia/Assets/Scripts/Runtime/LevelEvents/MasterStateManager.cs
+++ Paraphrenia/Assets/Scripts/Runtime/LevelEvents/MasterStateManager.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MasterStateManager : MonoBehaviour
+namespace Runtime.LevelEvents
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MasterStateManager : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
