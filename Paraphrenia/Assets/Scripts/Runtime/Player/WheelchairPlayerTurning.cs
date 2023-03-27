1071a0336938956e9303d8cc0483b4415027cf08
Calvin-Davidson <calvin2003davidson@gmail.com>
2023-03-27 11:57:17 +0200

    Basic wheelchair player rotation script

---
+++ Paraphrenia/Assets/Scripts/Runtime/Player/WheelchairPlayerTurning.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+namespace Runtime.Player
+{
+    public class WheelchairPlayerTurning : MonoBehaviour
+    {
+        [SerializeField] private float maxTurn = 20;
+        [SerializeField] private float turningSpeed = 2;
+
+        private float _currentRot = 0;
+        
+        private void Update()
+        {
+            float horizontalInput = -Input.GetAxis("Horizontal");
+
+            float rot = Mathf.Lerp(_currentRot, horizontalInput * maxTurn, Time.deltaTime * turningSpeed);
+
+            _currentRot = rot;
+            transform.rotation = Quaternion.Euler(new Vector3(0,0,_currentRot));
+        }
+    }
+}
