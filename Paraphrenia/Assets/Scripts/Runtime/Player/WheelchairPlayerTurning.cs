7fe8cb1ec62ea2191560b05e4d89790c3a7265b8
Calvin-Davidson <calvin2003davidson@gmail.com>
2023-03-27 12:44:13 +0200

    Added simple player turning forward/backward logic

--- Paraphrenia/Assets/Scripts/Runtime/Player/WheelchairPlayerTurning.cs
+++ Paraphrenia/Assets/Scripts/Runtime/Player/WheelchairPlayerTurning.cs
@@ -4,19 +4,22 @@ namespace Runtime.Player
 {
     public class WheelchairPlayerTurning : MonoBehaviour
     {
-        [SerializeField] private float maxTurn = 20;
+        [SerializeField] private float maxTurnZ = 20;
+        [SerializeField] private float maxTurnX = 20;
         [SerializeField] private float turningSpeed = 2;
 
-        private float _currentRot = 0;
+        private float _currentRotZ = 0;
+        private float _currentRotX = 0;
         
         private void Update()
         {
             float horizontalInput = -Input.GetAxis("Horizontal");
-
-            float rot = Mathf.Lerp(_currentRot, horizontalInput * maxTurn, Time.deltaTime * turningSpeed);
-
-            _currentRot = rot;
-            transform.rotation = Quaternion.Euler(new Vector3(0,0,_currentRot));
+            float verticalInput = Input.GetAxis("Vertical");
+            
+            _currentRotZ = Mathf.Lerp(_currentRotZ, horizontalInput * maxTurnZ, Time.deltaTime * turningSpeed);
+            _currentRotX = Mathf.Lerp(_currentRotX, verticalInput * maxTurnX, Time.deltaTime * turningSpeed);
+            
+            transform.rotation = Quaternion.Euler(new Vector3(_currentRotX,0,_currentRotZ));
         }
     }
 }
