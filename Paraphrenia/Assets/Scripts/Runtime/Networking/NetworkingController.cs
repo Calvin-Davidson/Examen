e1af6d56c05ab435930480ec79cb806324768ab9
Calvin-Davidson <calvin2003davidson@gmail.com>
2023-03-28 10:47:36 +0200

    Added force starting in the NetworkController

--- Paraphrenia/Assets/Scripts/Runtime/Networking/NetworkingController.cs
+++ Paraphrenia/Assets/Scripts/Runtime/Networking/NetworkingController.cs
@@ -9,6 +9,7 @@ namespace Runtime.Networking
     public class NetworkingController : MonoBehaviour
     {
         [SerializeField, Range(1, 2)] private int requiredPlayers = 2;
+        [SerializeField] private KeyCode forceStartKey = KeyCode.P;
         
         public UnityEvent onPlayerJoin = new();
         public UnityEvent onPlayerLeave = new();
@@ -18,6 +19,15 @@ namespace Runtime.Networking
         {
             NetworkManager.Singleton.OnClientConnectedCallback += HandlePlayerJoin;
             NetworkManager.Singleton.OnClientDisconnectCallback += HandlePlayerDisconnect;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(forceStartKey) && (!NetworkManager.Singleton.IsServer || NetworkManager.Singleton.IsClient))
+            {
+                NetworkManager.Singleton.StartHost();
+                NetworkManager.Singleton.SceneManager.LoadScene("PlayerMovement", LoadSceneMode.Single);
+            }
         }
 
         private void HandlePlayerJoin(ulong id)
