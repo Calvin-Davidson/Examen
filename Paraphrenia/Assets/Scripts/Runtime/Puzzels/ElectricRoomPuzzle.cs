e9024f4b640c4f9d29c8d1cf957d08328e5126e9
Calvin-Davidson <calvin2003davidson@gmail.com>
2023-04-17 09:50:50 +0200

    Created electric room puzzle logic

--- Paraphrenia/Assets/Scripts/Runtime/Puzzels/ElectricRoomPuzzle.cs
+++ Paraphrenia/Assets/Scripts/Runtime/Puzzels/ElectricRoomPuzzle.cs
@@ -6,21 +6,40 @@ namespace Runtime.Puzzels
 {
     public class ElectricRoomPuzzle : Puzzle
     {
-        [SerializeField] private NetworkedInteractable breakerInteractable;
+        [SerializeField, Space] private NetworkedInteractable breakerInteractable;
+        [SerializeField] private NetworkedInteractable[] interactableCables;
 
+        private int _repairedCables = 0;
+        private bool _isValid = false;
+        
         public UnityEvent onBreakerFailed;
         public UnityEvent onBreakerSuccess;
-        
-        public bool IsValid { get; set; } = false;
+        public UnityEvent onBecameValid;
+        public UnityEvent onBecameInvalid;
+
+        private bool IsValid
+        {
+            get => _isValid;
+            set
+            {
+                if (value) onBecameValid?.Invoke();
+                if (!value) onBecameInvalid?.Invoke();
+                _isValid = value;
+            }
+        }
 
         public override void OnNetworkSpawn()
         {
-            breakerInteractable.onInteract.AddListener(OnBreakerInteract);
+            breakerInteractable.onInteract.AddListener(HandleBreakerInteract);
+            foreach (var networkedInteractable in interactableCables)
+            {
+                networkedInteractable.onInteract.AddListener(HandleCableInteract);
+            }
             base.OnNetworkSpawn();
         }
         
 
-        private void OnBreakerInteract()
+        private void HandleBreakerInteract()
         {
             if (IsValid)
             {
@@ -30,6 +49,12 @@ namespace Runtime.Puzzels
             {
                 onBreakerFailed?.Invoke();
             }
+        }
+
+        private void HandleCableInteract()
+        {
+            _repairedCables += 1;
+            if (_repairedCables == interactableCables.Length - 1) IsValid = true;
         }
     }
 }
